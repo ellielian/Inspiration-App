2cb1b85aa64fbb3163a88c0d447aa9ef6fc4616f
Ellie Lian <xhlianellie@gmail.com>
2017-08-16 18:24:21 -0700

    Dev env setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports.getConfig = function(type) {
+
+  var isDev = type === 'development';
+
+  var config = {
+    entry: './app/scripts/main.js',
+    output: {
+      path: __dirname,
+      filename: 'main.js'
+    },
+    debug : isDev,
+    resolve: {
+      extensions: [".js", ".json", ".jsx"]
+    },
+    module: {
+      loaders: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }]
+    }
+  };
+
+  if(isDev){
+    config.devtool = 'eval';
+  }
+
+  return config;
+}
